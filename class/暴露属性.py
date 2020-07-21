f5d15223bca7c07114face8848e8f98d2e0aedd1
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-07-21 15:42:57 +0800

    重载运算

---
+++ class/暴露属性.py
@@ -0,0 +1,14 @@
+#既能检查参数，又可以用类似属性这样简单的方式来访问类的变量
+class Student (object):
+    @property
+    def score (self):
+        return self._score
+
+    @score.setter
+
+    def score (self,value):
+        if not isinstance(value, int):
+            raise ValueError('score must be an integer!')
+        if value < 0 or value > 100:
+            raise ValueError('score must between 0 ~ 100!')
+        self._score = value
