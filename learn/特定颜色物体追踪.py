74dc574159fe39226602421d0e0fe3bce14bc8f8
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-07-27 15:00:59 +0800

    合并多个图像显示

--- learn/特定颜色物体追踪.py
+++ learn/特定颜色物体追踪.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 # capture=cv2.VideoCapture(1)
 capture=cv2.imread('getblue.png')
 lower_blue=np.array([100,110,110])
@@ -12,7 +13,16 @@ mask=cv2.inRange(hsv,lower_blue,upper_blue)
 
 res=cv2.bitwise_and(frame,frame,mask=mask)
 
-cv2.imshow('frame',frame)
-cv2.imshow('mask',mask)
-cv2.imshow('res',res)
+# cv2.imshow('frame',frame)
+# cv2.imshow('mask',mask)
+# cv2.imshow('res',res)
+
+titles=['frame','mask','res']
+images=[frame,mask,res]
+for i in range(len(images)):
+    plt.subplot(2, 2, i + 1)
+    plt.imshow(cv2.cvtColor(eval(titles[i]), cv2.COLOR_BGR2RGB))
+    plt.title(titles[i], fontsize=8)
+    plt.xticks([]), plt.yticks([])
+plt.show()
 cv2.waitKey(0)
--- learn/颜色空间转换.py
+++ learn/颜色空间转换.py
@@ -1,11 +1,18 @@
 import cv2
-
+import matplotlib.pyplot as plt
 img = cv2.imread('lena.jpg')
 # # 转换为灰度图
 # B,G,R=cv2.split(img)
 
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # img_gray=cv2.merge((B*0.114,G*0.587,R*0.299))
-cv2.imshow('img', img)
-cv2.imshow('gray', img_gray)
+images=[img,img_gray]
+titles=['img','gray']
+for i in range(2):
+    plt.subplot(121+i)
+    plt.imshow(images[i],'gray')
+    plt.title(titles[i],fontsize=8)
+    plt.xticks([])
+    plt.yticks([])
+plt.show()
 cv2.waitKey(0)
