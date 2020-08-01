f421bd3b7a73036cee8e504efcacefc797529c2a
LYF <liuyuanfeng1@stu.gdou.edu.cn>
2020-08-01 14:51:07 +0800

    几何测试

---
+++ learn/2.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread('drawing.jpg')
+
+# 按照指定的宽度、高度缩放图片
+res = cv2.resize(img, (132, 150))
+# 按照比例缩放，如x,y轴均放大一倍
+res2 = cv2.resize(img, None, fx=2, fy=2, interpolation=cv2.INTER_LINEAR)
+
+cv2.imshow('shrink', res), cv2.imshow('zoom', res2)
+cv2.waitKey(0)
